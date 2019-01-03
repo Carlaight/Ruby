c1494fa4242c8a923657ac27dcf523be094b696a
charles <charlesmix23@gmail.com>
2019-01-03 19:33:55 +0100

    Exo 3 et 4

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+#Le # mets la ligne en commentaire
+
+# La différence et le retour à la ligne pour "puts" mais pas pour "print"
---
+++ exo_04.rb
@@ -0,0 +1,7 @@
+puts "Salut, ça farte ?
+
+#Voici ce que retourne le terminal :
+charles@charles:~/Desktop/rubythp$ ruby exo_04.rb
+exo_04.rb:2: unterminated string meets end of file
+
+# Il manque les guillemets après le point d'interrogation. La string n'est pas fermée.
